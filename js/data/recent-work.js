1db44da871f27fef478e1858f73df2d4cb7dbb4f
Eli-shamout <77059128+Eli-shamout@users.noreply.github.com>
2021-09-06 10:44:24 -0700

    Update recent-work.js

--- js/data/recent-work.js
+++ js/data/recent-work.js
@@ -2,37 +2,37 @@ let recentWorkData=[
     {
         id:0,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/01.jpg"
+        imageURL:"../../images/recent-work-images/01.jpg"
     },
     {
         id:1,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/02.jpg"
+        imageURL:"../../images/recent-work-images/02.jpg"
     },
     {
         id:2,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/03.jpg"
+        imageURL:"../../images/recent-work-images/03.jpg"
     },
     {
         id:3,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/04.jpg"
+        imageURL:"../../images/recent-work-images/04.jpg"
     },
     {
         id:4,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/05.jpg"
+        imageURL:"../../images/recent-work-images/05.jpg"
     },
     {
         id:5,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/06.jpg"
+        imageURL:"../../images/recent-work-images/06.jpg"
     },
     {
         id:6,
         title:"Eile",
-        imageURL:"C:/Users/Eile/Desktop/My WebSite/version 4 Final/images/recent-work-images/07.jpg"
+        imageURL:"../../images/recent-work-images/07.jpg"
     },
     
 ]
